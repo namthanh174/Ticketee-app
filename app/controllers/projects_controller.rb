1acc3e3d9aa24000e091a2e5e692441876334865
Vo Van Thanh <namthanh174@gmail.com>
2018-12-24 08:15:51 +0000

    Project can now updated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :project_params, only: [:create, :update]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
   def index
     @projects = Project.all
   end
@@ -19,12 +20,29 @@ class ProjectsController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been updated."
+      render 'edit'
+    end
+  end
+  
   def show
-    @project = Project.find_by(params[:id])
+    
   end
   
   private
     def project_params
       params.require(:project).permit(:name, :description)
+    end
+    
+    def set_project
+      @project = Project.find_by(params[:id])
     end
 end
